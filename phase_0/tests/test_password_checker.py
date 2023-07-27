fd2da1cf92390efc2c1ce61b7c8f6779c09dcd72
MichalSmyk <michal.smyk38@gmail.com>
2023-07-27 17:02:58 +0100

    add test cade for error

--- phase_0/tests/test_password_checker.py
+++ phase_0/tests/test_password_checker.py
@@ -4,3 +4,10 @@ from lib.password_checker import PasswordChecker
 def test_returns_true_if_password_is_more_or_8_char():
     password = PasswordChecker()
     assert password.check("12345678") == True
+
+def test_returns_err_msg_if_password_less_than_8_char():
+    password = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        password.check("12345")
+    error_msg = str(e.value)
+    assert error_msg == "Invalid password, must be 8+ characters."
