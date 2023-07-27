bf3a6b9576eb1391f07938fd3e7b7970cfd7b29d
MichalSmyk <michal.smyk38@gmail.com>
2023-07-27 14:27:58 +0100

    third test for challenge green

--- phase_0/tests/test_report_length.py
+++ phase_0/tests/test_report_length.py
@@ -7,3 +7,7 @@ def test_for_one_letter():
 def test_for_five_letter_word():
     result = report_length("house")
     assert result == "This string was 5 characters long."
+
+def test_for_no_word():
+    result = report_length("")
+    assert result == "This string was 0 characters long."
