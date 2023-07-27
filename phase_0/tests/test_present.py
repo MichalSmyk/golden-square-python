69201e6c533d0012ff428360e1c76426cd65e606
MichalSmyk <michal.smyk38@gmail.com>
2023-07-27 16:14:16 +0100

    add second test for error message if unwrap before wrap

--- phase_0/tests/test_present.py
+++ phase_0/tests/test_present.py
@@ -6,3 +6,14 @@ def test_wrap_and_unwrap():
     present = Present()
     present.wrap(33)
     assert present.unwrap() == 33
+
+"""
+unwrap before wrapping
+get error message 
+"""
+def test_unwrap_without_wrapping():
+    present = Present()
+    with pytest.raises(Exception) as e:
+        present.unwrap()
+    message = str(e.value)
+    assert message == "No contents have been wrapped."
