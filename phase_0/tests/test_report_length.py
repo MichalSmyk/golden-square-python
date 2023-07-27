79d24153d2b3e8337195ab70ffecc9c8785bbb80
MichalSmyk <michal.smyk38@gmail.com>
2023-07-27 14:26:52 +0100

    second test for challenge green

--- phase_0/tests/test_report_length.py
+++ phase_0/tests/test_report_length.py
@@ -3,3 +3,7 @@ from lib.report_length import report_length
 def test_for_one_letter():
     result = report_length("a")
     assert result == "This string was 1 characters long."
+
+def test_for_five_letter_word():
+    result = report_length("house")
+    assert result == "This string was 5 characters long."
