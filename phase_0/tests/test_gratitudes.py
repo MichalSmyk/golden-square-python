3feb080013f71f395f71d3f90c51cb7ef683f0a5
MichalSmyk <michal.smyk38@gmail.com>
2023-07-27 15:36:33 +0100

    third test for more than one grattitude

--- phase_0/tests/test_gratitudes.py
+++ phase_0/tests/test_gratitudes.py
@@ -10,3 +10,10 @@ def test_adds_one_gratitude():
     gratitudes = Gratitudes()
     gratitudes.add("weather")
     assert gratitudes.format() == "Be grateful for: weather"
+
+#adds two gratitudes and separates them with comma
+def test_daas_two_gratitudes_with_comma_separation():
+    gratitudes = Gratitudes()
+    gratitudes.add("weather")
+    gratitudes.add("food")
+    assert gratitudes.format() == "Be grateful for: weather, food"
