4139decc429c0b6f117cdbfbeedf2ccdb61ba24c
MichalSmyk <michal.smyk38@gmail.com>
2023-07-27 14:20:22 +0100

    second test second exercise passing

--- phase_0/tests/test_check_codeword.py
+++ phase_0/tests/test_check_codeword.py
@@ -3,3 +3,7 @@ from lib.check_codeword import *
 def test_with_correct_codeword():
     result = check_codeword("horse")
     assert result == "Correct! Come in."
+
+def test_with_incorrect_codeword():
+    result = check_codeword("water")
+    assert result == "WRONG!"
