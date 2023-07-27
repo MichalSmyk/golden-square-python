b663f96326e06192c3e7aaae7b28aa48049db352
MichalSmyk <michal.smyk38@gmail.com>
2023-07-27 15:03:34 +0100

    add comment for second test

--- phase_0/tests/test_string_builder.py
+++ phase_0/tests/test_string_builder.py
@@ -4,7 +4,7 @@ from lib.string_builder import *
 def test_string_builder_init_with_empty_string():
     builder = StringBuilder()
     assert builder.output() == ""
-
+#length for empty string
 def test_length_of_empty_string_is_zero():
     builder = StringBuilder()
     assert builder.size() == 0
