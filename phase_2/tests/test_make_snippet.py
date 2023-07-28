27bb65e01b25e60c8977555c3ee2156b3bd060d9
MichalSmyk <michal.smyk38@gmail.com>
2023-07-28 11:09:39 +0100

    second test passing

--- phase_2/tests/test_make_snippet.py
+++ phase_2/tests/test_make_snippet.py
@@ -4,3 +4,8 @@ from lib.make_snippet import *
 def test_given_empty_string_returns_it():
     result = make_snippet("")
     assert result == ""
+
+#given a string of one word returns it 
+def test_given_one_word_returns_it():
+    result = make_snippet("Hello")
+    assert result == "Hello"
