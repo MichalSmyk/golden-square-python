1aacb02ad946349422e806fc9cb22c153c123698
MichalSmyk <michal.smyk38@gmail.com>
2023-07-28 11:47:22 +0100

    challenge set up

---
+++ phase_2/tests/test_count_words.py
@@ -0,0 +1,8 @@
+from lib.count_words import count_words
+
+"""
+A function called count_words that takes a string as an argument and
+returns the number of words in that string.
+"""
+
+#takes empty string and returns it 
