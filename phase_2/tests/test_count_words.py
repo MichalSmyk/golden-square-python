60028ef1a91f443d48be71d3e5910096f6726785
MichalSmyk <michal.smyk38@gmail.com>
2023-07-28 11:50:48 +0100

    first test green

--- phase_2/tests/test_count_words.py
+++ phase_2/tests/test_count_words.py
@@ -6,3 +6,7 @@ returns the number of words in that string.
 """
 
 #takes empty string and returns it 
+def test_given_empty_string_returns_it():
+    result = count_words("")
+    assert result == ""
+
