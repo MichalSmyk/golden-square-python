7cc20f10387d2580049b7bf5d073fbf8ff3fe2da
MichalSmyk <michal.smyk38@gmail.com>
2023-07-28 14:06:27 +0100

    second test for 100 words added

--- phase_2/lib/reading_time.py
+++ phase_2/lib/reading_time.py
@@ -1,2 +1,6 @@
+
 def reading_time(text):
-    return 0
+    words_per_min = 200
+    words = len(text.split())
+    time = words / words_per_min
+    return time
--- phase_2/tests/test_reading_time.py
+++ phase_2/tests/test_reading_time.py
@@ -7,3 +7,11 @@ It returns how long it takes to read it when 200 words takes one minute
 def test_given_zero_words():
     result = reading_time("")
     assert result == 0
+
+"""
+Given 100 words
+It returns 0.5
+"""
+def test_given_100_words_returns_time_to_read():
+    result = reading_time("The a sun was setting behind the mountains casting a warm golden glow over the landscape Birds chirped their evening songs as they flew back to their nests A gentle breeze rustled the leaves of the trees creating a soothing melody People strolled along the winding paths enjoying the peaceful ambiance Children played in the park their laughter echoing in the air Nearby a stream flowed gently reflecting the colors of the setting sun The scent of blooming flowers filled the air adding to the enchanting atmosphere It was a picturesque evening one that would be cherished in memories forever")
+    assert result ==  0.5
