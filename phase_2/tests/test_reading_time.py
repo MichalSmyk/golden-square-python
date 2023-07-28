e9f4b572cab32e3ae85326cc30e1e7dd882627b3
MichalSmyk <michal.smyk38@gmail.com>
2023-07-28 14:09:02 +0100

    test for 20 words added

--- phase_2/tests/test_reading_time.py
+++ phase_2/tests/test_reading_time.py
@@ -15,3 +15,11 @@ It returns 0.5
 def test_given_100_words_returns_time_to_read():
     result = reading_time("The a sun was setting behind the mountains casting a warm golden glow over the landscape Birds chirped their evening songs as they flew back to their nests A gentle breeze rustled the leaves of the trees creating a soothing melody People strolled along the winding paths enjoying the peaceful ambiance Children played in the park their laughter echoing in the air Nearby a stream flowed gently reflecting the colors of the setting sun The scent of blooming flowers filled the air adding to the enchanting atmosphere It was a picturesque evening one that would be cherished in memories forever")
     assert result ==  0.5
+
+"""
+Given 200 words 
+It returns 1
+"""
+def test_given_200_words_returns_1():
+     result = reading_time("The a sun was setting behind the mountains casting a warm golden glow over the landscape Birds chirped their evening songs as they flew back to their nests A gentle breeze rustled the leaves of the trees creating a soothing melody People strolled along the winding paths enjoying the peaceful ambiance Children played in the park their laughter echoing in the air Nearby a stream flowed gently reflecting the colors of the setting sun The scent of blooming flowers filled the air adding to the enchanting atmosphere It was a picturesque evening one that would be cherished in memories forever The a sun was setting behind the mountains casting a warm golden glow over the landscape Birds chirped their evening songs as they flew back to their nests A gentle breeze rustled the leaves of the trees creating a soothing melody People strolled along the winding paths enjoying the peaceful ambiance Children played in the park their laughter echoing in the air Nearby a stream flowed gently reflecting the colors of the setting sun The scent of blooming flowers filled the air adding to the enchanting atmosphere It was a picturesque evening one that would be cherished in memories forever")
+     assert result ==  1
