a9ae4fa72d28713e6aab5e824631e729c3780f36
MichalSmyk <michal.smyk38@gmail.com>
2023-08-01 11:58:43 +0100

    all test passing

--- phase_2/lib/todo_checker.py
+++ phase_2/lib/todo_checker.py
@@ -1,5 +1,7 @@
 def todo_checker(text):
-    if text.upper().find("#TODO") != -1:
+    if text == None:
+        raise Exception("Please provide a string")
+    elif text.upper().find("#TODO") != -1:
         return True
     else: 
         return False
--- phase_2/tests/test_todo_checker.py
+++ phase_2/tests/test_todo_checker.py
@@ -1,5 +1,5 @@
 from lib.todo_checker import *
-
+import pytest
 """
 Given a text 
 It returns false
@@ -23,3 +23,38 @@ It returns true
 def test_text_contains_lowercase_todo():
     result = todo_checker("This string contains #todo")
     assert result == True
+
+"""
+Given a text without hash tag but uppercase
+It returns false
+"""
+def test_todo_in_upper_but_missing_hashtag():
+    result = todo_checker("This string contains wrong TODO")
+    assert result == False
+
+"""
+Given a text without hash tag but in lower case 
+It returns false
+"""
+def test_todo_in_lower_missing_tag():
+    result = todo_checker("This string contains wrong todo") 
+    assert result == False
+
+"""
+Given an empty string
+It returns false
+"""
+def test_with_empty_string():
+    result = todo_checker("")
+    assert result == False
+
+"""
+Given a None value
+It throws an error
+"""
+def test_given_a_non_value_throws_error():
+    # result = todo_checker(None)
+    with pytest.raises(Exception) as e:
+        todo_checker(None)
+    error_message = str(e.value)
+    assert error_message == "Please provide a string"
