231dc328850918641ab5b00cba96aa84d489e617
MichalSmyk <michal.smyk38@gmail.com>
2023-08-08 13:58:17 +0100

    second test passing

--- phase_2/tests/test_grammar_stats.py
+++ phase_2/tests/test_grammar_stats.py
@@ -8,3 +8,12 @@ def test_with_valid_sentence():
     grammar_stats = GrammarStats()
     result = grammar_stats.check("Hello, this is correct sentence.")
     assert result == True
+
+"""
+Given a sentence with capital letter but not fill stop or other mark
+Returns false 
+"""
+def test_with_capital_letter_but_no_end_mark():
+    grammar_stats = GrammarStats()
+    result = grammar_stats.check("Hello, this is incorrect sentence")
+    assert result == False
