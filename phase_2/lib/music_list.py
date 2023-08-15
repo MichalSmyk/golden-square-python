8a9549c2327d49bdd24afea22a71aa9729259343
MichalSmyk <michal.smyk38@gmail.com>
2023-08-15 12:51:21 +0100

    add third test, refactor code

--- phase_2/lib/music_list.py
+++ phase_2/lib/music_list.py
@@ -4,7 +4,8 @@ class MusicList:
         self._tracks = []
 
     def add_track(self, track):
-        self._tracks.append(track)
+        if track not in self._tracks:
+            self._tracks.append(track)
 
     def list_of_tracks(self):
        return self._tracks
--- phase_2/tests/test_music_list.py
+++ phase_2/tests/test_music_list.py
@@ -19,3 +19,14 @@ def test_adds_two_tracks():
     music_list.add_track("Artist one - track one")
     music_list.add_track("Artist two - track two")
     assert music_list.list_of_tracks() == ["Artist one - track one", "Artist two - track two"]
+
+"""
+Given a track that is alredy in the list 
+#add_track does not add it again to the list
+"""
+def test_add_track_does_not_dulpicate_tracks():
+    music_list = MusicList()
+    music_list.add_track("Artist one - track one")
+    music_list.add_track("Artist two - track two")
+    music_list.add_track("Artist two - track two")
+    assert music_list.list_of_tracks() == ["Artist one - track one", "Artist two - track two"]
