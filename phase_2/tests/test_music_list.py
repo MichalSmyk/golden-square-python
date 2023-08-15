c44ed4d1de62aafdbc487f356e4f7864e02c3bdc
MichalSmyk <michal.smyk38@gmail.com>
2023-08-15 12:46:42 +0100

    add second test and passing

--- phase_2/tests/test_music_list.py
+++ phase_2/tests/test_music_list.py
@@ -8,3 +8,14 @@ def test_adds_a_track_to_list():
     music_list = MusicList()
     music_list.add_track("Artist one - track one")
     assert music_list.list_of_tracks() == ["Artist one - track one"]
+
+
+"""
+Given two tracks 
+#add_track adds it to the list
+"""
+def test_adds_two_tracks():
+    music_list = MusicList()
+    music_list.add_track("Artist one - track one")
+    music_list.add_track("Artist two - track two")
+    assert music_list.list_of_tracks() == ["Artist one - track one", "Artist two - track two"]
