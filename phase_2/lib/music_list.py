c878f50d88f8eb5b01424304d88e3a112fc5a13c
MichalSmyk <michal.smyk38@gmail.com>
2023-08-15 12:44:17 +0100

    first test passing, add code to the class

--- phase_2/lib/music_list.py
+++ phase_2/lib/music_list.py
@@ -1,17 +1,10 @@
 class MusicList:
+    
+    def __init__(self):
+        self._tracks = []
 
     def add_track(self, track):
-        # Parameters:
-        #   track: string representing a single track
-        # Returns:
-        #   Nothing
-        # Side-effects
-        #   Saves the track to the self object
-        pass # No code here yet
+        self._tracks.append(track)
 
     def list_of_tracks(self):
-        # Returns:
-        #   an array with list of tracks
-        # Side-effects:
-        #   Throws an exception if no tracks
-        pass # No code here yet
+       return self._tracks
