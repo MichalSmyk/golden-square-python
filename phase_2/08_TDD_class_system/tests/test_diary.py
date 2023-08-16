bc282b688deb5023b2db2b7e0b4337966264c876
MichalSmyk <michal.smyk38@gmail.com>
2023-08-16 14:27:47 +0100

    add test for unit testing diary

--- phase_2/08_TDD_class_system/tests/test_diary.py
+++ phase_2/08_TDD_class_system/tests/test_diary.py
@@ -7,3 +7,11 @@ Initially has an empty list of entries
 def test_initially_has_empty_list_of_entries():
     diary = Diary()
     assert diary.all() == []
+
+"""
+Initially word count is zero
+"""
+
+def test_initially_word_count_is_zero():
+    diary = Diary()
+    assert diary.count_words() == 0
