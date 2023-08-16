25c25d3491407b555a3e11e79893ccc49a46f56e
MichalSmyk <michal.smyk38@gmail.com>
2023-08-16 14:02:38 +0100

    add test adds and lists diary entries"

---
+++ phase_2/08_TDD_class_system/tests/test_integration.py
@@ -0,0 +1,14 @@
+from lib.diary import Diary
+from lib.diary_entry import DiaryEntry
+"""
+Adds two diary entries
+I see them back in the list
+"""
+
+def test_adds_and_lists_diary_entries():
+    diary = Diary()
+    entry_1 = DiaryEntry("My Title 1", "My Contents 1")
+    entry_2 = DiaryEntry("My Title 2", "My Contents 2")
+    diary.add(entry_1)
+    diary.add(entry_2)
+    assert diary.all == [entry_1, entry_2]
