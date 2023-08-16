ae3ee81f98a49161e7d5e00b13374e8b6ff9cddb
MichalSmyk <michal.smyk38@gmail.com>
2023-08-16 14:09:38 +0100

    add integration test to count words

--- phase_2/08_TDD_class_system/tests/test_integration.py
+++ phase_2/08_TDD_class_system/tests/test_integration.py
@@ -15,5 +15,15 @@ def test_adds_and_lists_diary_entries():
 
 
 """
-
+Given I add two diary entries 
+And I call #count_words
+I get the sum of all words in the contents of diary entries 
 """
+
+def test_count_words_returns_count_of_all_words_in_all_entry_contents():
+    diary = Diary()
+    entry_1 = DiaryEntry("My Title 1", "one two three")
+    entry_2 = DiaryEntry("My Title 2", "four five six")
+    diary.add(entry_1)
+    diary.add(entry_2)
+    assert diary.count_words() == 6
