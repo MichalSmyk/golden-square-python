29d652d7a18bc8f0a7cc8b48a03349d1261e6d15
MichalSmyk <michal.smyk38@gmail.com>
2023-08-17 16:14:07 +0100

    add test

--- phase_2/08_TDD_class_system/tests/test_integration_todo.py
+++ phase_2/08_TDD_class_system/tests/test_integration_todo.py
@@ -2,6 +2,15 @@ from lib.todo import Todo
 from lib.todo_list import TodoList
 
 """
+Adds one todo to the list
+I can see it in the incomplete list
+"""
+def test_adds_one_todo_and_lists_it():
+    todo_list = TodoList()    
+    todo_1 = Todo("walk the dog")
+    todo_list.add(todo_1)
+    assert todo_list.incomplete() == [todo_1]
+"""
 Adds two todos 
 I can see them back in the incomplete list
 """
