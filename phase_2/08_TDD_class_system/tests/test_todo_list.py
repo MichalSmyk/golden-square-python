0a511361aaa3a52e4eb948163046ba36ee6b3882
MichalSmyk <michal.smyk38@gmail.com>
2023-08-21 14:07:09 +0100

    add unit test

--- phase_2/08_TDD_class_system/tests/test_todo_list.py
+++ phase_2/08_TDD_class_system/tests/test_todo_list.py
@@ -8,3 +8,10 @@ def test_initialises_with_empty_list():
     todo_list = TodoList()
     assert todo_list._todo == []
 
+"""
+Adds a task to incomplete list 
+"""
+def test_adds_a_task_to_incomplete_list():
+    todo_list = TodoList()
+    todo_list.add("walk the dog")
+    assert todo_list.incomplete() == ["walk the dog"]
