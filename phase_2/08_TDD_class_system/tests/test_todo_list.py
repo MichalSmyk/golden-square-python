a8345b453ae997e2a06d3ed5082137cafe411848
MichalSmyk <michal.smyk38@gmail.com>
2023-08-21 14:02:01 +0100

    Add unit test for todolist class

---
+++ phase_2/08_TDD_class_system/tests/test_todo_list.py
@@ -0,0 +1,9 @@
+from lib.todo_list import TodoList
+
+"""
+Initialises with empty todo list
+"""
+
+def test_initialises_with_empty_list():
+    todo_list = TodoList()
+    assert todo_list.incomplete() == []
