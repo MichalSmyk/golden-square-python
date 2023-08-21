0c823f1c0a57abc07758476eca280e988ebe79f3
MichalSmyk <michal.smyk38@gmail.com>
2023-08-21 13:55:36 +0100

    add second test for unit testing a class

--- phase_2/08_TDD_class_system/tests/test_todo.py
+++ phase_2/08_TDD_class_system/tests/test_todo.py
@@ -8,3 +8,11 @@ def test_initialises_with_a_task_set_to_false():
     todo = Todo("clean room")
     assert todo.complete == False
 
+"""
+sets the complete to True
+"""
+
+def test_sets_the_complete_to_true():
+    todo = Todo("walk the cat")
+    todo.mark_complete()
+    assert todo.complete == True
