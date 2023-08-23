9be7adc63321580277cc44feb38960df138b6095
MichalSmyk <michal.smyk38@gmail.com>
2023-08-23 14:39:59 +0100

    add integration test

---
+++ phase_3/01_unit_testing_parent_child_classes/tests/test_ingetration.py
@@ -0,0 +1,12 @@
+"""
+When I add multiple tracks 
+They are reflected in the tracks list
+"""
+
+def test_adds_and_lists_multiple_tracks():
+    library = MusicLibrary()
+    track_1 = Track("My Title 1", "My Artist 1")
+    track_2 = Track("My Title 2", "My Artist 2")
+    library.add(track_1)
+    library.add(track_2)
+    assert library.tracks == [track_1, track_2]
