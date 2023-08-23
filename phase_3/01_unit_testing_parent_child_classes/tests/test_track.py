850152a4545b247942a8584122739aa3470e9df8
MichalSmyk <michal.smyk38@gmail.com>
2023-08-23 15:30:01 +0100

    add test for partial artist

--- phase_3/01_unit_testing_parent_child_classes/tests/test_track.py
+++ phase_3/01_unit_testing_parent_child_classes/tests/test_track.py
@@ -26,3 +26,12 @@ Matches is true
 def test_matches_on_full_artist():
     track = Track("Cat Title", "Dog Artist")
     assert track.matches("Dog Artist") == True
+
+"""
+Given a title and an artist
+And a search keyword for a partial artist 
+Matches is true
+"""
+def test_matches_on_partial_title():
+    track = Track("Cat Title", "Dog Artist")
+    assert track.matches("Dog") == True
