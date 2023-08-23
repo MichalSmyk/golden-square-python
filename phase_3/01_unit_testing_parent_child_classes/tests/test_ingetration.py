4a62b22666fdcd594494a0838797051c85dbd6a6
MichalSmyk <michal.smyk38@gmail.com>
2023-08-23 14:45:43 +0100

    add second integration test for search by title

--- phase_3/01_unit_testing_parent_child_classes/tests/test_ingetration.py
+++ phase_3/01_unit_testing_parent_child_classes/tests/test_ingetration.py
@@ -13,3 +13,18 @@ def test_adds_and_lists_multiple_tracks():
     library.add(track_1)
     library.add(track_2)
     assert library.tracks == [track_1, track_2]
+
+
+"""
+When I add multiple tracks
+And I search for a track title
+Then I get the matching track back
+"""
+
+def test_searches_for_track_by_full_title():
+    library = MusicLibrary()
+    track_1 = Track("Dog", "Cat")
+    track_2 = Track("Cow", "Goat")
+    library.add(track_1)
+    library.add(track_2)
+    assert library.search("Dog") == [track_1]
