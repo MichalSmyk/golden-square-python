b4989726ec4ed0196213932913a4795b54915bf9
MichalSmyk <michal.smyk38@gmail.com>
2023-08-23 15:19:27 +0100

    add code in search funtion, test pass

--- phase_3/01_unit_testing_parent_child_classes/lib/music_library.py
+++ phase_3/01_unit_testing_parent_child_classes/lib/music_library.py
@@ -8,6 +8,7 @@ class MusicLibrary:
         self.tracks.append(track)
 
     def search(self, keyword):
-        # keyword is a string
-        # Returns a list of instances of track that match the keyword
-        pass
+        return [
+            track for track in self.tracks
+            if track.matches(keyword)
+        ]
