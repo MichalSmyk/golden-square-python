9f0ccf11e4fb8df4ced177339259ab727c5379c5
MichalSmyk <michal.smyk38@gmail.com>
2023-08-23 15:26:25 +0100

    add unit test for partial title

--- phase_3/01_unit_testing_parent_child_classes/tests/test_track.py
+++ phase_3/01_unit_testing_parent_child_classes/tests/test_track.py
@@ -6,5 +6,14 @@ Matches is true
 """
 
 def test_matches_on_full_title():
-    track = Track("Cat", "Dog")
-    assert track.matches("Cat") == True
+    track = Track("Cat Title", "Dog Artist")
+    assert track.matches("Cat Title") == True
+
+"""
+Given a title and an artist
+And a search keyword for a partial title 
+Matches is true
+"""
+def test_matches_on_partial_title():
+    track = Track("Cat Title", "Dog Artist")
+    assert track.matches("Cate") == True
