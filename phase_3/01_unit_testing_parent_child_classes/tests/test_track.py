653283748ebb75dd75608dc7a8dbd17cc32a6387
MichalSmyk <michal.smyk38@gmail.com>
2023-08-23 15:28:39 +0100

    add test for matches for full artist

--- phase_3/01_unit_testing_parent_child_classes/tests/test_track.py
+++ phase_3/01_unit_testing_parent_child_classes/tests/test_track.py
@@ -17,3 +17,12 @@ Matches is true
 def test_matches_on_partial_title():
     track = Track("Cat Title", "Dog Artist")
     assert track.matches("Cat") == True
+
+"""
+Given a title and an artist
+And a search keyword for a full artist 
+Matches is true
+"""
+def test_matches_on_full_artist():
+    track = Track("Cat Title", "Dog Artist")
+    assert track.matches("Dog Artist") == True
