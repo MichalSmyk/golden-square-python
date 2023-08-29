d4f6dc38bcedb1904b34c20ba1a164f42db049b5
MichalSmyk <michal.smyk38@gmail.com>
2023-08-29 14:43:17 +0100

    add challenge for mock testing

---
+++ phase_3/02_crafting_mocks/tests/test_mock_chellenge.py
@@ -0,0 +1,10 @@
+def test_creates_a_sophisticated_mock():
+    # Uncomment and set up your mocks here
+    # task_list = 
+    # task = 
+
+    # Don't edit below
+    task_list.add(task)
+    assert task_list.list() == [task]
+    assert task_list.count() == 1
+    assert task_list.clear() == "success"
