52f57a21668f09bf2ef46ad0e5e97d8c26a13f94
MichalSmyk <michal.smyk38@gmail.com>
2023-08-30 14:18:39 +0100

    test passing

--- phase_3/04_unit_test_API_request/tests/test_time_error.py
+++ phase_3/04_unit_test_API_request/tests/test_time_error.py
@@ -1,5 +1,12 @@
 from lib.time_error import TimeError
+from unittest.mock import Mock
 
 def test_returns_difference_between_local_and_remote_times():
-    time_error = TimeError()
-    assert time_error.error() == 0.1
+    requester_mock = Mock()
+    response_mock = Mock()
+    requester_mock.get.return_value = response_mock
+    response_mock.json.return_value = {"unixtime": 1693400891}
+    timer_mock = Mock()
+    timer_mock.time.return_value = 1693401132.5
+    time_error = TimeError(requester_mock, timer_mock)
+    assert time_error.error() == -241.5
